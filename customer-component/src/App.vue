ff74166aae0c06af691b381190daece1d9a4116a
Emanuel Fabbro <fabbroemanuel7@gmail.com>
2024-04-22 05:00:51 -0300

    created new component customer-list

---
+++ customer-component/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <CustomerList />
+  </div>
+</template>
+
+<script>
+import CustomerList from './components/CustomerList.vue';
+
+export default {
+  components: {
+    CustomerList
+  }
+};
+</script>
---
+++ customer-component/src/components/CustomerList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Customer List</h2>
+    <input
+      type="text"
+      v-model="newCustomer"
+      placeholder="Enter customer name"
+      @keyup.enter="addCustomer"
+    />
+    <button @click="addCustomer">Add Customer</button>
+    <select v-if="customers.length > 0">
+      <option disabled value="">Select a customer</option>
+      <option v-for="customer in customers" :key="customer">{{ customer }}</option>
+    </select>
+    <p v-else>No customers added yet.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newCustomer: '',
+      customers: []
+    };
+  },
+  methods: {
+    addCustomer() {
+      if (this.newCustomer.trim() !== '') {
+        this.customers.push(this.newCustomer.trim());
+        this.newCustomer = ''; // Clear input after adding customer
+      }
+    }
+  }
+};
+</script>
+
+<style>
+/* Add styles as needed */
+</style>
